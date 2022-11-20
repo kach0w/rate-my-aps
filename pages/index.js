fefc9bb1527b95bb56fa6c70bda930703ded320f
Kachow <karsab343@gmail.com>
2022-11-19 18:45:31 -0800

    commented out <link> in index.js

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
         <title>kach0w</title>
         <meta name="description" content="kach0w's website"></meta>
         <link rel="icon" href="/favicon.png"/>  
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" />
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>     
-        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" rel="stylesheet" /> */}
       </Head>
       {/* Navbar */}
       <Navbar />
