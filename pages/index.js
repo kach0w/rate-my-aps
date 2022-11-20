146aa64072705bc16b74d08595a84ba5ca701f99
Kachow <karsab343@gmail.com>
2022-11-19 18:11:38 -0800

    changed link

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
         <meta name="description" content="kach0w's website"></meta>
         <link rel="icon" href="/favicon.png"/>  
         {/* <link href="../styles/fonts.css" rel="stylesheet" /> */}
-        <style>
+        {/* <style>
         @import url('https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900');
-        </style>
-        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova" rel="stylesheet" />
+        </style> */}
+        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" rel="stylesheet" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
         <script src="https://kit.fontawesome.com/a191de05ec.js" crossorigin="anonymous"></script>
