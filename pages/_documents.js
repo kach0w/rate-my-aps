43ad6cccf07c5b4c99e0ca9e4a1e2d73308fdadc
Kachow <karsab343@gmail.com>
2022-11-19 20:00:39 -0800

    commented out old google font links

--- pages/_documents.js
+++ pages/_documents.js
@@ -6,10 +6,10 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"/>
-        
+{/*         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@400;600;700;900" rel="stylesheet" /> */}
       </Head>
       <body>
         <Main />
