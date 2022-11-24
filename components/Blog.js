a0da5b7c0840696aeb37b246fee05703b4eb1557
Kachow <karsab343@gmail.com>
2022-11-23 21:23:49 -0800

    fixed home page blog section and footer

--- components/Blog.js
+++ components/Blog.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className='pt-[4vh] justify-center text-center h-[100vh] w-[100vw]'>
+    <div className='pt-[4vh] justify-center text-center w-[100vw]'>
         <div className='pt-5'>
             <h1 id="about" className='font-serif font-bold text-[7em] text-[#15171a]'>Blog</h1>
         </div>
-        <div className='mx-auto w-[50vw] h-[50vh] rounded-lg border-5 border-black p-5 bg-white drop-shadow-lg font-serif font-normal text-[2rem]'>
+        <div className='mx-auto max-w-[50vw] rounded-lg p-5 bg-white drop-shadow-xl  font-serif font-normal text-[2rem]'>
             <h2 className='font-serif font-bold'>Statistics</h2>
-            <h2 className='font-serif font-normal text-[1rem]'>Sep 4, 2022</h2>
-            <div className='pb-[2vh] pt-[2vh]'>
+            <h2 className='font-serif font-bold text-[1rem]'>Sep 4, 2022</h2>
+            <div className='pb-5 pt-5'>
                 Sorry, I haven't posted here in a while. I was still working on more Github projects over the summer but I forgot to update what I've done here. Anyways, school started and in my stats class because we were only going to start using calculators next week I wanted to make something to graph and display data... 
             </div>
             <a className='bg-[#3b82f6] rounded-lg text-white mt-[2vh] hover:bg-[#0a5adb] p-3' href="/blog/statistics">Read more</a>
         </div>
-        <div className='text-[2rem] mt-4 hover:text-slate-500'>
+        <div className='text-[2rem] mt-4 mb-4 hover:text-slate-500'>
             <a href="/blog"><u>More Blog Posts</u></a>
         </div>
     </div>
--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-[16vh] bg-[#3b82f6] content-center justify-center align-middle text-white text-3xl font-bold'>
-        <div class="float-left ml-[15vw] mt-[5vh] align-middle hover:text-slate-300">
+        <div class="float-left ml-[15vw] mt-[5vh] align-middle">
             <p>Fremont, California</p>
         </div>
-        <div class="float-right mr-[15vw] mt-[5vh]  hover:text-slate-300">
-            <a className='hover:text-slate-300' href="https://github.com/kach0w"><i class="fab fa-github"></i> GitHub · </a>
-            <a className=' hover:text-slate-300' href="mailto:kachow@duck.com"><i class="fa-solid fa-envelope"></i> Email</a>
+        <div class="float-right mr-[15vw] mt-[5vh]">
+            <a className='hover:text-slate-800' href="https://github.com/kach0w"><i class="fab fa-github"></i> GitHub · </a>
+            <a className=' hover:text-slate-800' href="mailto:kachow@duck.com"><i class="fa-solid fa-envelope"></i> Email</a>
         </div>
     </div>
   )
