a1dba384120400b912e53bff369940394e0201e0
Kachow <karsab343@gmail.com>
2023-01-22 16:27:44 -0800

    rem

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-[12vh] bg-[#f3f3f3] text-slate-500 text-sm font-normal'>
+    <div className='mt-[9vh] bg-[#f3f3f3] text-slate-500 text-sm font-normal'>
         <div class="float-left ml-[40vw] mt-[5vh] align-middle">
             <p>Fremont, California</p>
         </div>
         <div class="float-right mr-[40vw] mt-[5vh]">
-            <a className='text-[#3b82f6] hover:underline' href="https://github.com/kach0w">GitHub</a> ·
+            <a className='text-[#3b82f6] hover:underline' href="https://github.com/kach0w">GitHub </a>·
             {/* <a className='hover:text-slate-300' href="https://github.com/kach0w"><i class="fab fa-github"></i> GitHub</a> ·  */}
             <a className=' text-[#3b82f6] hover:underline' href="mailto:kachow@duck.com"> Email</a>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import rusteze from '../assets/rusteze-2.png';
 
 export default function Home() {
   return (
-    <div className='w-[100vw] h-[100vh] bg-[#f3f3f3] text-center'>
+    <div className='w-[100vw] text-[#222] h-[100vh] bg-[#f3f3f3] text-center'>
       <Head>
         <title>kach0w</title>
         <meta name="description" content="kach0w's website"></meta>
@@ -18,28 +18,26 @@ export default function Home() {
       </Head>
       {/* Navbar  color should be bg-[#141414] */}
       {/* <Navbar /> */}
-      <h2 className='font-bold text-3xl mt-[3vh] mb-[3vh]'>kach0w</h2>
-      <div className='w-[25vw] h-[18vh] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto mb-[3vh]'>
-          <div className='w-[9vw] mt-[2vh] float-left'>
-            <Image className='w-[8vw]' src={rusteze} alt="img"/>
-            {/* <div className='font-bold text-[12px]'>
-                Trying to Learn New Things.
-            </div> */}
-            
+      <h2 className='font-extrabold  text-3xl mt-[3vh] mb-[1vh]'>kach0w</h2>
+      <div className='mb-[2vh]'>
+        Trying to Learn New Things.
+      </div>
+      <div className='w-[30rem] md:flex align-middle rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto mb-[3vh]'>
+          <div className='align-middle float-left'>
+            <Image className='w-[12rem]' src={rusteze} alt="img"/>
           </div>
-          <div className='w-[15vw] float-right mt-[1vw] mr-[1vw] text-center'>
+          <div className='w-[16rem] mt-[1rem] align-middle float-right text-center'>
             Hello, welcome to my website.
-            <br>
-            </br>
-            Check out some of my <a className='text-[#3b82f6] hover:underline' href="/projects">projects</a> and feel free to also view my <a className='text-[#3b82f6] hover:underline' href="/blog">blog</a> where I try (and mostly fail) to frequently give an update on new things I am learning. 
+            <br></br>
+            Check out some of my <a className='text-[#3b82f6] hover:underline' href="/projects">projects</a> and also view the <a className='text-[#3b82f6] hover:underline' href="/blog">blog</a> which I try (and mostly fail) to frequently update . 
           </div>
       </div>
       <h2 className='font-bold text-2xl underline mb-1'>About Me</h2>
-      <div className='w-[25vw] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh]'>
-        I am a high schooler trying to learn new things in Computer Science. Right now I am trying to go into the backend of web development and also want to learn more about machine learning in web applications. 
+      <div className='w-[30rem] md:flex rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh]'>
+        I am a high schooler trying to learn new things in Computer Science. Right now I am trying to learn backend also want to learn more about machine learning in web applications. 
       </div>  
       <h2 className='font-bold text-2xl underline mb-1'>Contact</h2>
-      <div className='w-[25vw] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh]'>
+      <div className='w-[30rem] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh]'>
         <div>
           <ul>
             <li>
@@ -54,8 +52,8 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <h2 className='font-bold text-2xl underline mb-1'>Latest Blog Post</h2>
-      <div className='w-[25vw] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh] text-left'>
+      <h2 className='font-bold text-2xl underline'>Latest Blog Post</h2>
+      <div className='w-[30rem] rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh] text-left'>
         <p className='font-bold text-slate-900'>Statistics with ChartJS</p>
         <p className='text-slate-600'>Implementing things I learned in my Statistics class with the Javascript graphing tool ChartJS.</p>
         <p className=' mt-2 text-sm text-slate-400'>September 4, 2022</p>
