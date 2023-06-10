7043ce78f42769951a58bf9c1fac3fd331fe01e9
Kachow <karsab343@gmail.com>
2023-06-10 16:17:15 -0700

    Co-authored-by: Anilkumar Sarvabatla <Mybobksf@users.noreply.github.com>

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import logo from '../assets/favicon.ico'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Script from 'next/script';
+import Head from 'next/head'
 
 const Navbar = () => {
   return (
-    <div className='justify-center content-center text-center m-x-auto'>
-        <div className='z-0 flex text-[1.5rem] text-white pt-5 space-x-20 h-15 text-center justify-center content-center align-middle z-3 font-serif'> 
-            <a className = "hover:text-slate-300" href="https://kach0w.com">Home</a>
-            <a className = "hover:text-slate-300" href="#about">About</a>
-            <a className = "hover:text-slate-300" href="#projects">Projects</a> 
-            <a className = "hover:text-slate-300" href="#blog">Blog</a>
+    <div className='text-[#222] bg-[#f3f3f3] text-center font-[Inter] flex space-x-6'>
+        <div className='w-[100vw] z-0 text-[1.5rem] text-white pt-5 h-15 align-middle z-3 font-serif'> 
+          <a href="../" className='inline-block align-middle float-left pl-5'>
+            <div className='w-[20rem] text-[#222] '>
+              <Image className='inline-block align-middle w-[5rem] h-[5rem]' src={logo}></Image> 
+              <h2 className='inline-block align-middle font-black text-[2rem]'>Rate My AP's</h2>
+            </div>
+          </a>
+          <div className='float-right pr-40'>
+            <a className='text-[#222] inline-block align-middle font-black pt-[0.36rem] pr-5' href="../help">AP Help</a>
+            <a className='text-[#222] inline-block align-middle font-black pt-[0.36rem] pr-5' href="../team">Team</a>
+            <a className='text-[#222] inline-block align-middle font-black pt-[0.36rem] pr-5' href="../about">About</a>
+            <a className='text-[#222] inline-block align-middle font-black pt-[0.36rem]' href="../contact">Contact</a>
 
+          </div>
+          
         </div>
     </div>
   )
