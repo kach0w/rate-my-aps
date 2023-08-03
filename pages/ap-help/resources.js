460725b25582bd20d858c8d62fa712eee9574edb
Kachow <karsab343@gmail.com>
2023-08-02 18:41:33 -0700

    dropdown list

--- pages/ap-help/resources.js
+++ pages/ap-help/resources.js
@@ -28,13 +28,37 @@ const resource = ({docname}) => {
   return (
     <div>
       <Navbar />
-      {/* <div className='text-center'>
+      <div className='text-center'>
         <select value={selectedOption} onChange={handleOptionChange} className='px-6 py-2 bg-[white] rounded-md border-2'>
           <option value="chemistry">Chemistry</option>
           <option value="biology">Biology</option>
+          <option value="environmentalscience">Environmental Science</option>
+          <option value="physicsone">Physics 1</option>
           <option value="physicstwo">Physics 2</option>
+          <option value="physicscmechanics">Physics C Mechanics</option>
+          <option value="physicsceandm">Physics C E&M</option>
+          <option value="computersciencea">Computer Science A</option>
+          <option value="computersciencep">Computer Science P</option>
+          <option value="calculusab">Calculus AB</option>
+          <option value="calculusbc">Calculus BC</option>
+          <option value="statistics">Statistics</option>
+          <option value="ushistory">US History</option>
+          <option value="worldhistory">World History</option>
+          <option value="eurohistory">European History</option>
+          <option value="usgovt">US Government</option>
+          <option value="humangeo">Human Geography</option>
+          <option value="arthistory">Art History</option>
+          <option value="musictheory">Music Theory</option>
+          <option value="seminar">Seminar</option>
+          <option value="psychology">Psychology</option>
+          <option value="literature">Literature</option>
+          <option value="lang">Language</option>
+          <option value="microeconomics">Microeconomics</option>
+          <option value="macroeconomics">Macroeconomics</option>
+
+
         </select>
-      </div> */}
+      </div>
       <Resource key={selectedOption} docname={selectedOption}></Resource>
 
 
